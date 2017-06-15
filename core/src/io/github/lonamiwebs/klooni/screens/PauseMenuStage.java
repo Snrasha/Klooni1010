a3af02401afd39ee8bca70a7d94f7939edaeaab1
root <root@a2b6fa06b050>
2017-06-15 13:09:58 +0000

    Commit Insert Annotations

--- core/src/io/github/lonamiwebs/klooni/screens/PauseMenuStage.java
+++ core/src/io/github/lonamiwebs/klooni/screens/PauseMenuStage.java
@@ -1,141 +1,94 @@
-/*
-    1010! Klooni, a free customizable puzzle game for Android and Desktop
-    Copyright (C) 2017  Lonami Exo | LonamiWebs
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 package io.github.lonamiwebs.klooni.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-
 import io.github.lonamiwebs.klooni.Klooni;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import io.github.lonamiwebs.klooni.Theme;
 import io.github.lonamiwebs.klooni.actors.Band;
-import io.github.lonamiwebs.klooni.actors.SoftButton;
 import io.github.lonamiwebs.klooni.game.BaseScorer;
+import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
+import Gdx.gl;
+import Interpolation.swingOut;
+import Interpolation.swingIn;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import ShapeRenderer.ShapeType.Filled;
+import GL20.GL_BLEND;
+import io.github.lonamiwebs.klooni.actors.SoftButton;
+import Gdx.input;
 import io.github.lonamiwebs.klooni.game.GameLayout;
+import com.badlogic.gdx.InputProcessor;
+import Gdx.graphics;
+import Input.Keys;
 
-// The pause stage is not a whole screen but rather a menu
-// which can be overlaid on top of another screen
 class PauseMenuStage extends Stage {
-
-    //region Members
-
     private InputProcessor lastInputProcessor;
+
     private boolean shown;
+
     private boolean hiding;
 
     private final ShapeRenderer shapeRenderer;
 
     private final Klooni game;
+
     private final Band band;
+
     private final BaseScorer scorer;
+
     private final SoftButton playButton;
 
-    //endregion
-
-    //region Constructor
-
-    // We need the score to save the maximum score if a new record was beaten
     PauseMenuStage(final GameLayout layout, final Klooni game, final BaseScorer scorer, final int gameMode) {
         this.game = game;
         this.scorer = scorer;
-
-        shapeRenderer = new ShapeRenderer(20); // 20 vertex seems to be enough for a rectangle
-
+        shapeRenderer = new ShapeRenderer(20);
         Table table = new Table();
         table.setFillParent(true);
         addActor(table);
-
-        // Current and maximum score band.
-        // Do not add it to the table not to over-complicate things.
         band = new Band(game, layout, this.scorer);
         addActor(band);
-
-        // Home screen button
         final SoftButton homeButton = new SoftButton(3, "home_texture");
         table.add(homeButton).space(16);
-
         homeButton.addListener(new ChangeListener() {
-            public void changed (ChangeEvent event, Actor actor) {
+            public void changed(ChangeEvent event, Actor actor) {
                 game.transitionTo(new MainMenuScreen(game));
             }
         });
-
-        // Replay button
         final SoftButton replayButton = new SoftButton(0, "replay_texture");
         table.add(replayButton).space(16);
-
         replayButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                // false, don't load the saved game state; we do want to replay
                 game.transitionTo(new GameScreen(game, gameMode, false));
             }
         });
-
         table.row();
-
-        // Palette button (buy colors)
         final SoftButton paletteButton = new SoftButton(1, "palette_texture");
         table.add(paletteButton).space(16);
-
         paletteButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                // Don't dispose because then it needs to take us to the previous screen
                 game.transitionTo(new CustomizeScreen(game, game.getScreen()), false);
             }
         });
-
-        // Continue playing OR share (if game over) button
         playButton = new SoftButton(2, "play_texture");
         table.add(playButton).space(16);
-
         playButton.addListener(playChangeListener);
     }
 
-    //endregion
-
-    //region Private methods
-
-    // Hides the pause menu, setting back the previous input processor
     private void hide() {
         shown = false;
         hiding = true;
-        Gdx.input.setInputProcessor(lastInputProcessor);
-
-        addAction(Actions.sequence(
-                Actions.moveTo(0, Gdx.graphics.getHeight(), 0.5f, Interpolation.swingIn),
-                new RunnableAction() {
-                    @Override
-                    public void run() {
-                        hiding = false;
-                    }
-                }
-        ));
+        input.setInputProcessor(lastInputProcessor);
+        addAction(Actions.sequence(Actions.moveTo(0, graphics.getHeight(), 0.5F, swingIn), new RunnableAction() {
+            @Override
+            public void run() {
+                hiding = false;
+            }
+        }));
         scorer.resume();
     }
 
@@ -146,39 +99,27 @@ class PauseMenuStage extends Stage {
         }
     };
 
-    //endregion
-
-    //region Package local methods
-
-    // Shows the pause menu, indicating whether it's game over or not
     void show() {
         scorer.pause();
         scorer.saveScore();
-
-        // Save the last input processor so then we can return the handle to it
-        lastInputProcessor = Gdx.input.getInputProcessor();
-        Gdx.input.setInputProcessor(this);
+        lastInputProcessor = input.getInputProcessor();
+        input.setInputProcessor(this);
         shown = true;
         hiding = false;
-
-        addAction(Actions.moveTo(0, Gdx.graphics.getHeight()));
-        addAction(Actions.moveTo(0, 0, 0.75f, Interpolation.swingOut));
+        addAction(Actions.moveTo(0, graphics.getHeight()));
+        addAction(Actions.moveTo(0, 0, 0.75F, swingOut));
     }
 
     void showGameOver(final String gameOverReason, final boolean timeMode) {
-        if (game.shareChallenge != null) {
+        if ((game.shareChallenge) != null) {
             playButton.removeListener(playChangeListener);
             playButton.updateImage("share_texture");
             playButton.addListener(new ChangeListener() {
                 public void changed(ChangeEvent event, Actor actor) {
-                    // Don't dispose because then it needs to take us to the previous screen
-
-                    game.transitionTo(new ShareScoreScreen(
-                            game, game.getScreen(), scorer.getCurrentScore(), timeMode), false);
+                    game.transitionTo(new ShareScoreScreen(game, game.getScreen(), scorer.getCurrentScore(), timeMode), false);
                 }
             });
         }
-
         band.setMessage(gameOverReason);
         show();
     }
@@ -191,34 +132,26 @@ class PauseMenuStage extends Stage {
         return hiding;
     }
 
-    //endregion
-
-    //region Public methods
-
     @Override
     public void draw() {
         if (shown) {
-            // Draw an overlay rectangle with not all the opacity
-            // This is the only place where ShapeRenderer is OK because the batch hasn't started
-            Gdx.gl.glEnable(GL20.GL_BLEND);
-            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            gl.glEnable(GL_BLEND);
+            shapeRenderer.begin(Filled);
             Color color = new Color(Klooni.theme.bandColor);
-            color.a = 0.1f;
+            color.a = 0.1F;
             shapeRenderer.setColor(color);
-            shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            shapeRenderer.rect(0, 0, graphics.getWidth(), graphics.getHeight());
             shapeRenderer.end();
         }
-
         super.draw();
     }
 
     @Override
     public boolean keyUp(int keyCode) {
-        if (keyCode == Input.Keys.P || keyCode == Input.Keys.BACK) // Pause
+        if ((keyCode == (Keys.P)) || (keyCode == (Keys.BACK)))
             hide();
-
+        
         return super.keyUp(keyCode);
     }
-
-    //endregion
 }
+
